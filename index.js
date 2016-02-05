63b06f30ffe2e25199ade25390cc3098f923f857
Ilya Erokhin <i.erokhin@icloud.com>
2016-02-05 10:37:34 +0300

    fist transform

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
+// todo: pipes!
+
 const CONTAINERS = {
   vek: {
     root: 'http://www.vek-dverey.ru',
     transforms: {
       big: 'big',
       medium: 'medium',
-      small: 'small'
+      small: 'convert -define png:size=240x180 {source} -thumbnail 120x90 {destination}'
     }
   }
-}
+};
 
 const PORT = 9988;
 const STORAGE_ROOT = `${__dirname}/STORAGE`;
@@ -19,13 +21,14 @@ const http = require('http');
 const request = require('request');
 const fs = require('fs');
 const path = require('path');
-const static = require('node-static');
+const nodeStatic = require('node-static');
+require('shelljs/global');
 
 mkdirp.sync(STORAGE_ROOT);
 
 // request: {service_url}/vek/big/klin/impex1.png
 
-var staticServer = new static.Server(STORAGE_ROOT);
+var staticServer = new nodeStatic.Server(STORAGE_ROOT);
 
 http.createServer(function(request, response) {
   request.addListener('end', function () {
@@ -40,13 +43,14 @@ http.createServer(function(request, response) {
             response.end();
             return;
           }
-          action(r);
-          // response.writeHead(200, {"Content-Type": "text/plain"});
-          // response.write('dummy');
-          // response.end();
-          relative = r.locals[r.transName].replace(STORAGE_ROOT, '');
-          console.log(relative);
-          staticServer.serveFile(relative, 200, {}, request, response);
+          action(r, function() {
+            // response.writeHead(200, {"Content-Type": "text/plain"});
+            // response.write('dummy');
+            // response.end();
+            relative = r.locals[r.transName].replace(STORAGE_ROOT, '');
+            console.log(relative);
+            staticServer.serveFile(relative, 200, {}, request, response);
+          });
         }
     });
   }).resume();
@@ -54,9 +58,9 @@ http.createServer(function(request, response) {
 
 function requestParser(request) {
   // requestInfo
-  var r = {}
+  var r = {};
 
-  var parts = request.url.split('/')
+  var parts = request.url.split('/');
   r.container = parts[1];
   r.transName = parts[2];
   r.relativePath = '/' + parts.slice(3, parts.lenght).join('/');
@@ -68,36 +72,46 @@ function requestParser(request) {
     r.transform = CONTAINERS[r.container].transforms[r.transName];
   }
 
-  console.log(r);
   if (!(r.container && r.transName && r.relativePath && r.transform)) {
     throw new Error();
   }
 
-  r.locals = {}
+  r.locals = {};
   r.locals.origin = `${STORAGE_ROOT}/${r.container}/origin${r.relativePath}`;
   r.locals[r.transName] = `${STORAGE_ROOT}/${r.container}/${r.transName}${r.relativePath}`;
 
   return r;
 }
 
-function action(r) {
+function action(r, callback) {
   console.log(r);
   if (r.transName === 'origin') {
     console.log('Original img');
-    pipe = getOrigin(r);
+    getOrigin(r, callback);
+  } else {
+    getOrigin(r, function() {
+      console.log(r.transform);
+      var source = r.locals.origin;
+      var dest = r.locals[r.transName];
+      var destDirname = path.dirname(dest);
+      mkdirp.sync(destDirname);
+      var transform = r.transform.replace('{source}', source).replace('{destination}', dest);
+      exec(transform);
+      callback();
+    });
   }
 }
 
-function getOrigin(r) {
+function getOrigin(r, callback) {
   if (fileExists(r.locals.origin)) {
     console.log('file exist');
+    callback();
   } else {
-    dirname = path.dirname(r.locals.origin);
+    var dirname = path.dirname(r.locals.origin);
     mkdirp.sync(dirname);
     request(r.origin).pipe(
       fs.createWriteStream(r.locals.origin)
-    );
-    console.log('file created');
+    ).on('finish', callback);
   }
 }
 
