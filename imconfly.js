96f72a0d2cffbb1fece40080b8560f72cc0bcfd2
Ilya Erokhin <i.erokhin@icloud.com>
2016-03-25 08:15:56 +0300

    some code improvements

--- imconfly.js
+++ imconfly.js
@@ -3,22 +3,23 @@
 // todo: SAFETY CHECK!
 // todo: params for transforms validation
 
-var mkdirp = require('mkdirp');
-var http = require('http');
-var request = require('request');
 var fs = require('fs');
+var http = require('http');
 var path = require('path');
+
+var mkdirp = require('mkdirp');
+var request = require('request');
 var nodeStatic = require('node-static');
 var shell = require('shelljs');
 
 module.exports = function(conf) {
   var staticServer = new nodeStatic.Server(conf.storage_root);
 
-  function requestParser(request) {
+  function urlParser(url) {
     // requestInfo
     var r = {};
 
-    var parts = request.url.split('/');
+    var parts = url.split('/');
     r.container = parts[1];
     r.transName = parts[2];
     r.relativePath = '/' + parts.slice(3, parts.lenght).join('/');
@@ -58,9 +59,6 @@ module.exports = function(conf) {
             callback(err);
           }
           var transform = r.transform.replace('{source}', source).replace('{destination}', dest);
-          // *******************************
-          // * 99.9% of perfomance is here *
-          // *******************************
           shell.exec(transform, {async: true, silent: true}, function (code, stdout, stderr) {
             if (code) {
               console.error('Exit code:', code);
@@ -103,32 +101,35 @@ module.exports = function(conf) {
     });
   }
 
-  var app = http.createServer(function (request, response) {
+  return http.createServer(function (request, response) {
     request.addListener('end', function () {
-      staticServer.serve(request, response, function (e, res) {
-        if (e && e.status == 404) {
-          try {
-            var r = requestParser(request);
-          } catch (err) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.end('HTTP 404 - Not Found');
-            return;
-          }
-          action(r, function (err) {
-            if (err) {
-              response.writeHead(404, {"Content-Type": "text/plain"});
-              response.end(`HTTP 404 - Not Found. (Request to origin error: ${err.message})`);
-            } else {
-              var relative = r.locals[r.transName].replace(conf.storage_root, '');
-              staticServer.serveFile(relative, 200, {}, request, response);
+      staticServer.serve(request, response, function (e) {
+        if (e) {
+          if (e.status == 404) {
+            try {
+              var r = urlParser(request.url);
+            } catch (err) {
+              response.writeHead(404, {'Content-Type': 'text/plain'});
+              response.end('HTTP 404 - Not Found');
+              return;
             }
-          });
+            action(r, function (err) {
+              if (err) {
+                response.writeHead(404, {'Content-Type': 'text/plain'});
+                response.end(`HTTP 404 - Not Found. (Request to origin error: ${err.message})`);
+              } else {
+                var relative = r.locals[r.transName].replace(conf.storage_root, '');
+                staticServer.serveFile(relative, 200, {}, request, response);
+              }
+            });
+          } else {
+            response.writeHead(e.status, {'Content-Type': 'text/plain'});
+            response.end(e.message);
+          }
         }
       });
     }).resume();
   });
-
-  return app;
 };
 
 if (!module.parent) {
