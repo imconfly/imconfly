044aa493e27f2ef7065370e14355f93474f868b0
Ilya Erokhin <sniffsla@gmail.com>
2017-02-19 10:02:48 +0300

    code structure improvements in Conf

--- app/conf.js
+++ app/conf.js
@@ -11,7 +11,7 @@ const common = require("./common");
 /**
  * Configuration object
  *
- * @property {string} workdir
+ * @property {string} workDir
  * @property {string} storageRoot
  * @property {number} port
  * @property {object} urlChecker
@@ -20,87 +20,20 @@ const common = require("./common");
 class Conf {
   /**
    *
-   * @param {Object} obj
-   * @param {string} workdir
+   * @param  {Object}      obj
+   * @param  {string}      workDir
    * @throws {ConfError}
    */
-  constructor(obj, workdir) {
-    // this.workdir
-    let st;
-    try {
-      st = fs.statSync(workdir);
-    } catch (e) {
-      throw new ConfError(`"${workdir}" no such file or directory`);
-    }
-    if (!st.isDirectory()) {
-      throw new ConfError(`"${workdir}" is not a directory`);
-    }
-
-    this.workdir = path.resolve(workdir);
-
-    // this.storageRoot
+  constructor(obj, workDir) {
     if (!obj || typeof obj !== "object") {
       throw new ConfError("Given conf is not an object");
     }
-    if (obj.storageRoot === undefined) {
-      this.storageRoot = path.resolve(this.workdir, DEFAULT_STORAGE_DIRNAME);
-    } else {
-      if (typeof obj.storageRoot !== "string") {
-        throw new ConfError(`"storageRoot" param is not a string`);
-      }
-      if (obj.storageRoot.length === 0) {
-        throw new ConfError(`"storageRoot" param is an empty string`);
-      }
-      this.storageRoot = path.resolve(obj.storageRoot);
-    }
 
-    // this.port
-    if (obj.port === undefined) {
-      this.port = DEFAULT_PORT
-    } else if (typeof obj.port !== "number") {
-      throw new ConfError(`"port" param is not a number`);
-    } else {
-      this.port = obj.port;
-    }
-
-    // this.urlChecker
-    if (obj.urlChecker === undefined) {
-      this.urlChecker = DEFAULT_URL_CHECK;
-    } else {
-      if (typeof obj.urlChecker === "object") {
-        this.urlChecker = obj.urlChecker;
-      } else if (typeof obj.urlChecker === "string") {
-        let match = obj.urlChecker.match(new RegExp('^/(.*?)/([gimy]*)$'));
-        this.urlChecker = new RegExp(match[1], match[2]);
-      } else {
-        throw new ConfError('"urlChecker" param is not a regexp or string');
-      }
-    }
-
-    // this.containers
-    if (obj.containers === undefined) {
-      throw new ConfError(`"containers" param missed`);
-    }
-    if (typeof obj.containers !== "object") {
-      throw new ConfError(`"containers" param is not an object`);
-    }
-    if (Object.keys(obj.containers).length === 0) {
-      throw new ConfError(`"containers" param is an empty object`);
-    }
-    this.containers = {};
-    for (let key of Object.keys(obj.containers)) {
-      let container = obj.containers[key];
-      if (container.root === undefined) {
-        throw new ConfError(`param "root" not found in container ${key}`)
-      }
-      if (typeof container.root !== "string") {
-        throw new ConfError(`param "root" in container ${key} is not a string`)
-      }
-      this.containers[key] = {root: container.root};
-      if (container.transforms !== undefined && Object.keys(container.transforms).length > 0) {
-        this.containers[key].transforms = container.transforms;
-      }
-    }
+    this.workDir = this._workDir(workDir);
+    this.storageRoot = this._storageRoot(obj);
+    this.port = this._port(obj);
+    this.urlChecker = this._urlChecker(obj);
+    this.containers = this._containers(obj);
   }
 
   /**
@@ -114,6 +47,91 @@ class Conf {
     let dirname = path.dirname(filePath);
     let obj = require(filePath);
     return new Conf(obj, dirname);
+  }
+
+  _workDir(workDir) {
+    let st;
+    try {
+      st = fs.statSync(workDir);
+    } catch (e) {
+      throw new ConfError(`"${workDir}" no such file or directory`);
+    }
+    if (!st.isDirectory()) {
+      throw new ConfError(`"${workDir}" is not a directory`);
+    }
+
+    return path.resolve(workDir);
+  }
+
+  _storageRoot(obj) {
+    if (obj.storageRoot === undefined) {
+      return path.resolve(this.workDir, DEFAULT_STORAGE_DIRNAME);
+    }
+
+    if (typeof obj.storageRoot !== "string") {
+      throw new ConfError(`"storageRoot" param is not a string`);
+    }
+    if (obj.storageRoot.length === 0) {
+      throw new ConfError(`"storageRoot" param is an empty string`);
+    }
+
+    return path.resolve(obj.storageRoot);
+  }
+
+  _port(obj) {
+    if (obj.port === undefined) {
+      return DEFAULT_PORT
+    }
+    if (typeof obj.port !== "number") {
+      throw new ConfError(`"port" param is not a number`);
+    }
+
+    return obj.port;
+  }
+
+  _urlChecker(obj) {
+    if (obj.urlChecker === undefined) {
+      return DEFAULT_URL_CHECK;
+    }
+
+    if (typeof obj.urlChecker === "object") {
+      return obj.urlChecker;
+    }
+
+    if (typeof obj.urlChecker === "string") {
+      let match = obj.urlChecker.match(new RegExp('^/(.*?)/([gimy]*)$'));
+      return new RegExp(match[1], match[2]);
+    } else {
+      throw new ConfError('"urlChecker" param is not a regexp or string');
+    }
+  }
+
+  _containers(obj) {
+    if (obj.containers === undefined) {
+      throw new ConfError(`"containers" param missed`);
+    }
+    if (typeof obj.containers !== "object") {
+      throw new ConfError(`"containers" param is not an object`);
+    }
+    if (Object.keys(obj.containers).length === 0) {
+      throw new ConfError(`"containers" param is an empty object`);
+    }
+
+    let containers = {};
+    for (let key of Object.keys(obj.containers)) {
+      let container = obj.containers[key];
+      if (container.root === undefined) {
+        throw new ConfError(`param "root" not found in container ${key}`)
+      }
+      if (typeof container.root !== "string") {
+        throw new ConfError(`param "root" in container ${key} is not a string`)
+      }
+      containers[key] = {root: container.root};
+      if (container.transforms !== undefined && Object.keys(container.transforms).length > 0) {
+        containers[key].transforms = container.transforms;
+      }
+    }
+    return containers;
   }
 }
 
